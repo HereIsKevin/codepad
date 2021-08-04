754934e2064513d4ea81d2ab787475ba0d62f148
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-08-04 12:31:24 -0700

    Add a build script for creating binaries

---
+++ build.py
@@ -0,0 +1,22 @@
+import os
+import platform
+import subprocess
+
+if platform.system() == "Darwin":
+    icon = "./resources/codepad.icns"
+else:
+    icon = "./resources/codepad.ico"
+
+subprocess.run(
+    [
+        "pyinstaller",
+        "./codepad/__main__.py",
+        "--windowed",
+        "--name",
+        "Codepad",
+        "--icon",
+        icon,
+        "--add-data",
+        f"./resources/codepad.png{os.path.pathsep}./resources/",
+    ]
+)
